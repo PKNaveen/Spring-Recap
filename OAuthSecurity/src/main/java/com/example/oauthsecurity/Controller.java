1c9b8eff0d27c2c3fac8fa381028908a877c95d8
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2024-03-28 11:31:30 +0530

    Oauth Basics done

---
+++ OAuthSecurity/src/main/java/com/example/oauthsecurity/Controller.java
@@ -0,0 +1,20 @@
+package com.example.oauthsecurity;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello, Home!!";
+    }
+
+    @GetMapping("/secured")
+    public String secured(){
+        return "Hello, Secured!!";
+    }
+
+}
---
+++ OAuthSecurity/src/test/java/com/example/oauthsecurity/OAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
